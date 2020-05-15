bf8db45ca31c73335bcb619c1f309850dbe49e65
Seb <sebba@end.ro>
2020-05-15 11:11:48 +0300

    removing macropump - checking sensors when need it by specific scripts

--- _Doc/Arduino Modbus/RackModbusInputs/RackModbusInputs.ino
+++ _Doc/Arduino Modbus/RackModbusInputs/RackModbusInputs.ino
@@ -11,13 +11,14 @@ u16 _D[DATA_SIZE];
 #define SLAVE_ID 2
 #define DE_RE_PIN 13
 
-ModbusRTUSlave rtu(SLAVE_ID, &Serial, DE_RE_PIN);
+ModbusRTUSlave rtu(SLAVE_ID, &Serial1, DE_RE_PIN);
 
 void setup()
 { 
   rtu.addWordArea(VIRTUAL_ADDRESS, _D, DATA_SIZE);
   rtu.begin(9600);
 
+  pinMode(A0, INPUT_PULLUP);  // set pull-up on analog pin 0 
   pinMode(2, INPUT_PULLUP);
   pinMode(3, INPUT_PULLUP);
   pinMode(4, INPUT_PULLUP);
@@ -28,28 +29,45 @@ void setup()
   pinMode(9, INPUT_PULLUP);
   pinMode(10, INPUT_PULLUP);
   pinMode(11, INPUT_PULLUP);
-  pinMode(A0, INPUT_PULLUP);  // set pull-up on analog pin 0 
 
-  //Serial.begin(9600); // not needed, for logging purpose only
+  Serial.begin(9600); // not needed, for logging purpose only
 
 }
 
 void loop()
 {
   // set some value in data array to test if master can read and modify it
-  _D[0] = digitalRead(2);
-  _D[1] = digitalRead(3);
-  _D[2] = digitalRead(4);
-  _D[3] = digitalRead(5);
-  _D[4] = digitalRead(6);
-  _D[5] = digitalRead(7);
-  _D[6] = digitalRead(8);
-  _D[7] = digitalRead(9);
-  _D[8] = digitalRead(10);
-  _D[9] = digitalRead(11);
-  _D[10] = analogRead(A0);
+  _D[0] = analogRead(A0);
+    delay(10);
+  _D[1] = digitalRead(2);
+    delay(10);
+  _D[2] = digitalRead(3);
+    delay(10);
+  _D[3] = digitalRead(4);
+    delay(10);
+  _D[4] = digitalRead(5);
+    delay(10);
+  _D[5] = digitalRead(6);
+    delay(10);
+  _D[6] = digitalRead(7);
+    delay(10);
+  _D[7] = digitalRead(8);
+    delay(10);
+  _D[8] = digitalRead(9);
+    delay(10);
+  _D[9] = digitalRead(10);
+    delay(10);
+  _D[10] = digitalRead(11);
+    delay(10);
 
+  for (int i = 0; i <= 10; i++) {  
+  Serial.print(_D[i]);
+  if (i==0) {Serial.print(" - ");} else {Serial.print(" ");}
+}
+  Serial.println("");
+  
   // waiting for requests from master
   // reading and writing _D according to requests from master happens here
   rtu.process();
+    delay(10);
 }
